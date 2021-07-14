132e62fc27fb55ac42ecc411250568f457abc932
Marcin <marcin.gier@wp.pl>
2021-07-14 21:45:02 +0200

    daoimpl

--- src/main/java/CRM/DAO/CustomerDAOImpl.java
+++ src/main/java/CRM/DAO/CustomerDAOImpl.java
@@ -8,35 +8,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
-public class CustomerDAOImpl implements CustamerDAO{
-    // need to inject the session factory
+public class CustomerDAOImpl implements CustamerDAO {
     @Autowired
     private SessionFactory sessionFactory;
 
     @Override
     public List<Customer> getCustomers() {
 
-        // get the current hibernate session
         Session currentSession = sessionFactory.getCurrentSession();
 
-        // create a query  ... sort by last name
         Query<Customer> theQuery =
                 currentSession.createQuery("from Customer order by lastName", Customer.class);
 
-        // execute query and get result list
         List<Customer> customers = theQuery.getResultList();
 
-        // return the results
         return customers;
     }
 
     @Override
     public void saveCustomer(Customer theCustomer) {
 
-        // get current hibernate session
         Session currentSession = sessionFactory.getCurrentSession();
 
-        // save/upate the customer ... finally LOL
         currentSession.saveOrUpdate(theCustomer);
 
     }
@@ -44,10 +37,8 @@ public class CustomerDAOImpl implements CustamerDAO{
     @Override
     public Customer getCustomer(int theId) {
 
-        // get the current hibernate session
         Session currentSession = sessionFactory.getCurrentSession();
 
-        // now retrieve/read from database using the primary key
         Customer theCustomer = currentSession.get(Customer.class, theId);
 
         return theCustomer;
@@ -56,10 +47,8 @@ public class CustomerDAOImpl implements CustamerDAO{
     @Override
     public void deleteCustomer(int theId) {
 
-        // get the current hibernate session
         Session currentSession = sessionFactory.getCurrentSession();
 
-        // delete object with primary key
         org.hibernate.query.Query theQuery =
                 currentSession.createQuery("delete from Customer where id=:customerId");
         theQuery.setParameter("customerId", theId);
